b9ca6fde110ccc460d44c5ef5e4d39a4327e1a16
NO411 <weiler.noah2006@gmail.com>
2023-03-19 21:50:05 +0100

    basic new file dialog

--- src/aboutbox.py
+++ src/aboutbox.py
@@ -45,8 +45,8 @@ class Ui_AboutBox(object):
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt; color:#06a0ff;\">Notensatzprogramm</span></p>\n"
-"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:11pt; color:#06a0ff;\">Notensatzprogramm</span></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:10pt;\"><br /></p>\n"
 "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Author: Noah Weiler</p>\n"
 "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Version: 0.0.1</p>\n"
 "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>"))
--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from PyQt5.QtWidgets import *
 import qdarktheme
 from mainwindow import Ui_MainWindow
 from aboutbox import Ui_AboutBox
+from new_document import Ui_NewDocumentDialog
 
 __author__ = "Noah Weiler"
 
@@ -16,15 +17,18 @@ qdarktheme.setup_theme()
 
 window = QMainWindow()
 aboutbox = QDialog()
-
+new_doc_dialog = QDialog()
 
 ui = Ui_MainWindow()
 ui.setupUi(window)
 
 aboutbox_ui = Ui_AboutBox()
 aboutbox_ui.setupUi(aboutbox)
+new_doc_dialog_ui = Ui_NewDocumentDialog()
+new_doc_dialog_ui.setupUi(new_doc_dialog)
 
 ui.action_about.triggered.connect(aboutbox.show)
+ui.action_new.triggered.connect(new_doc_dialog.show)
 
 window.show()
 app.exec()
