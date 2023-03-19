c01af41ec972f419ff8f8bb35285f91a497ea5e8
NO411 <weiler.noah2006@gmail.com>
2023-03-19 19:24:35 +0100

    add basic aboutbox

--- src/main.py
+++ src/main.py
@@ -3,16 +3,28 @@ main.py: Notensatzprogramm, grundlegende Programmstruktur
 """
 
 from PyQt5.QtWidgets import *
+import qdarktheme
 from mainwindow import Ui_MainWindow
-
+from aboutbox import Ui_AboutBox
 
 __author__ = "Noah Weiler"
 
 app = QApplication([])
+
+# apply the dark theme to the app
+qdarktheme.setup_theme()
+
 window = QMainWindow()
+aboutbox = QDialog()
+
+
 ui = Ui_MainWindow()
-
 ui.setupUi(window)
-window.show()
 
+aboutbox_ui = Ui_AboutBox()
+aboutbox_ui.setupUi(aboutbox)
+
+ui.action_about.triggered.connect(aboutbox.show)
+
+window.show()
 app.exec()
