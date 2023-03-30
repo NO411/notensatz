ea08f6a7df6d1b9a5821cfba3cacc27f51901088
NO411 <weiler.noah2006@gmail.com>
2023-03-30 23:15:21 +0200

    simplify button selection function

--- src/main.py
+++ src/main.py
@@ -36,7 +36,7 @@ def main():
 
     for n_class, _ in enumerate(app.symbols):
         for i, symbol_button in enumerate(app.ui.symbols_box_buttons[n_class]):
-            app.ui.symbols_box_buttons[n_class][i].clicked.connect(lambda n_class=n_class, index=i: ui_misc.unselect_buttons(n_class, index))
+            symbol_button.clicked.connect(lambda n_class=n_class, index=i: ui_misc.unselect_buttons(n_class, index))
 
     app.window.show()
     app.app.exec()
